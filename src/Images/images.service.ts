31271859b10892fc06b50dee4515fe6580f20950
ArcadiaIsAway <megacraftng500@outlook.com>
2025-10-21 09:52:44 -0300

    Arcammit

--- src/Images/images.service.ts
+++ src/Images/images.service.ts
@@ -6,22 +6,22 @@ export class ImagesService {
   constructor(private readonly prisma: PrismaService) {}
 
   create(data: { base64: string; altText?: string | null; url?: string | null }) {
-    const db = this.prisma as unknown as { img: { create: Function } };
-    return db.img.create({ data });
+    const db = this.prisma as unknown as { Img: { create: Function } };
+    return db.Img.create({ data });
   }
 
   findAll() {
-    const db = this.prisma as unknown as { img: { findMany: Function } };
-    return db.img.findMany({ orderBy: { id: 'asc' } });
+    const db = this.prisma as unknown as { Img: { findMany: Function } };
+    return db.Img.findMany({ orderBy: { id: 'asc' } });
   }
 
   findOne(id: number) {
-    const db = this.prisma as unknown as { img: { findUnique: Function } };
-    return db.img.findUnique({ where: { id } });
+    const db = this.prisma as unknown as { Img: { findUnique: Function } };
+    return db.Img.findUnique({ where: { id } });
   }
 
   remove(id: number) {
-    const db = this.prisma as unknown as { img: { delete: Function } };
-    return db.img.delete({ where: { id } });
+    const db = this.prisma as unknown as { Img: { delete: Function } };
+    return db.Img.delete({ where: { id } });
   }
 }
